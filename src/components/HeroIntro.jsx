1ecdcd8bf460ba03e6856a2e298f7d73289caf2f
Mahadi200 <shurov895@gmail.com>
2025-06-30 22:13:18 +0600

    HydroJan

--- src/components/HeroIntro.jsx
+++ src/components/HeroIntro.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-const text = " DoB-HYDROযান";
+const text = " DoB-HYDROJAN";
 
 const letterVariants = {
   hidden: { opacity: 0, y: 50 },
