112167075802578eba0ab528b439fbaea0add635
Mahadi200 <shurov895@gmail.com>
2025-07-01 00:15:17 +0600

    added footer

--- src/pages/Robosuf.jsx
+++ src/pages/Robosuf.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import HydroJan01 from './HydroJan01';
+import HydroJan02 from './HydroJan02';
+import HydroJan03 from './HydroJan03';
+
 const Robosuf = () => {
   return (
     <div>
-      dsfesrfe
+      <HydroJan03/>
+      <HydroJan02/>
+      <HydroJan01/>
+     
     </div>
   )
 }
