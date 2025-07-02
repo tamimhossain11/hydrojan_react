16c59c73ffcc25fa094de39aaedfd22328c9f5d0
Mahadi200 <shurov895@gmail.com>
2025-07-02 12:22:32 +0600

    added AUV

---
+++ src/pages/AUV.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HydroJan03 from './HydroJan03'
+
+const AUV = () => {
+  return (
+    <div>
+        <HydroJan03/>
+      
+    </div>
+  )
+}
+
+export default AUV
